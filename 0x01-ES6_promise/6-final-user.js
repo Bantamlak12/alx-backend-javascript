70c95005002f6f871504c56c57d8231e001774eb
Bantamlak <bantamlak29@gmail.com>
2024-01-05 10:31:04 +0300

    string literal used

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,6 +7,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled([signupPromise, uploadPromise]).then((res) => res.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : `${result.reason}`,
   })));
 }
