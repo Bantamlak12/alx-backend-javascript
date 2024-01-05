5ec22fb4fc787e5437a7d7409013588f8b3bdb59
Bantamlak <bantamlak29@gmail.com>
2024-01-05 10:10:20 +0300

    promise returned

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([uploadedRes, userRes]) => {
       const { body: photo } = uploadedRes;
       const { firstName, lastName } = userRes;
