d0dbb60bdb94519e0ea5f1ee1008337d26edc16b
Bantamlak <bantamlak29@gmail.com>
2024-01-05 09:54:38 +0300

    solution added

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([uploadedRes, userRes]) => {
+      const { body: photo } = uploadedRes;
+      const { firstName, lastName } = userRes;
+
+      console.log(`${photo} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
