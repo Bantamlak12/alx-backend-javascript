149760eed5c881417eeb5daa12b50f01ba1cec6d
Bantamlak <bantamlak29@gmail.com>
2024-01-09 18:00:05 +0300

    comparing 5 with the current value

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -6,11 +6,11 @@ export function queryAPI(endpoint) {
   // Check if the endpoint is in the WeakMap or do has a value or set a value
   if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
 
-  //   Get the value of the count from the endpoint key
+  // Get the value of the count from the endpoint key
   const count = weakMap.get(endpoint);
 
   // Increament the counter for the endpoint
   weakMap.set(endpoint, count + 1);
 
-  if (count >= 5) throw new Error('Endpoint load is high');
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 }
