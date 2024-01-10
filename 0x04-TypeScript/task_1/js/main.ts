079bdb2c7f668713cfe96cbe2807163af4f61e89
Bantamlak <bantamlak29@gmail.com>
2024-01-10 18:52:12 +0300

    taks 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Task 1
 interface Teacher {
   firstName: string;
   lastName: string;
@@ -5,10 +6,6 @@ interface Teacher {
   yearsOfExperience?: number;
   readonly location: string;
   [key: string]: any;
-}
-
-interface Directors extends Teacher {
-  numberOfReports: number;
 }
 
 const teacher3: Teacher = {
@@ -21,6 +18,11 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
+// Taks 2
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const director1: Directors = {
   firstName: "John",
   lastName: "Doe",
@@ -29,3 +31,14 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+// Task 3
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+printTeacher("John", "Doe");
