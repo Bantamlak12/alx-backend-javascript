5c663bb4631ce5e636af381d343f00c16cd36797
Bantamlak <bantamlak29@gmail.com>
2024-02-26 16:50:19 +0300

    task 3: async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,54 @@
+const fs = require('fs').promises;
+
+const countStudents = async (path) => {
+  try {
+    // check if the file exists
+    await fs.access(path);
+
+    // Read if the file exists
+    const dataPromise = await fs.readFile(path);
+    const data = dataPromise.toString();
+
+    const lines = data.split('\n');
+
+    let totalStudent = 0;
+    const studentByField = {};
+    const names = {};
+
+    for (let i = 1; i < lines.length; i += 1) {
+      if (lines[i] !== '') {
+        const fields = lines[i].split(',');
+        const field = fields[3].trim();
+        const firstName = fields[0].trim();
+
+        totalStudent += 1;
+        if (studentByField[field] === undefined) {
+          studentByField[field] = 1;
+          names[field] = [firstName];
+        } else {
+          studentByField[field] += 1;
+          names[field].push(firstName);
+        }
+      }
+    }
+
+    console.log(`Number of students: ${totalStudent}`);
+    for (const field in studentByField) {
+      if (field) {
+        console.log(
+          `Number of students in ${field}: ${
+            studentByField[field]
+          }. List: ${names[field].join(', ')}`,
+        );
+      }
+    }
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('Cannot load the database');
+    } else {
+      console.log(err);
+    }
+  }
+};
+
+module.exports = countStudents;
