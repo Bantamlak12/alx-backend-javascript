f7e78651b90a20316bd02ecd178e59fe5ff661f4
Bantamlak <bantamlak29@gmail.com>
2024-02-26 20:50:30 +0300

    task 5: revert changes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,14 +2,13 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const PORT = 1245;
-
-const path = process.argv[2];
+const PATH = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents(path)
+    countStudents(PATH)
       .then((data) => {
         res.write('This is the list of our students\n');
         res.end(data.trim());
