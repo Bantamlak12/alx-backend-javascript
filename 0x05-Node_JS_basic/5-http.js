69787ab4fdfe3a6f45e934611e20525df98192d3
Bantamlak <bantamlak29@gmail.com>
2024-02-26 21:30:01 +0300

    task 5: plain text returned

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,17 +6,22 @@ const PATH = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     countStudents(PATH)
       .then((data) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('This is the list of our students\n');
         res.end(data.trim());
       })
       .catch((err) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end(err.message);
       });
   }
+  // res.writeHead(404, { 'Content-Type': 'text/plain' });
+  // res.end();
 });
 
 app.listen(PORT);
