b89c36544f20bac27583598522051c4611e7b7ed
Bantamlak <bantamlak29@gmail.com>
2024-02-29 10:00:56 +0300

    task 6: passed done to it

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,12 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function () {
-  it('should return a promise', function () {
+  it('should return a promise', function (done) {
     getPaymentTokenFromAPI(true)
       .then((result) => {
-        expect(result).to.deep.equal({data: 'Successful response from the API' });
+        expect(result).to.deep.equal({
+          data: 'Successful response from the API',
+        });
         done();
       })
       .catch((err) => {
